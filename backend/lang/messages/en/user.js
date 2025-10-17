835a38fa12d84e19ef9f38be0420afb47808d6f4
MarcusLages <marcusvlages@gmail.com>
2025-10-17 14:05:36 -0700

    Added user facing messages from backend

---
+++ backend/lang/messages/en/user.js
@@ -0,0 +1,6 @@
+exports.MSGS = {
+    DEFAULT_BAD_REQ_ERR: "Bad request.",
+    DEFAULT_NOT_FOUND_ERR: "Page not found.",
+    INVALID_QUERY_ERR: "Bad request. Invalid SQL query.",
+    FORBIDDEN_QUERY_ERR: "Forbidden. Only accepts %1 queries.",
+}
