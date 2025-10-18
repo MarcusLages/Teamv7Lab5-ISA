4d13cce74a29e612dd53dfe4264fde82aaa6169f
hugoamuan <hugo.amuan@icloud.com>
2025-10-18 15:52:01 -0700

    implement frontend client with GET/POST sql handling matching backend API

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -5,7 +5,7 @@ const submit_query_btn = document.getElementById("submit_query")
 const sql_query = document.getElementById("sql_query");
 const server_response_div = document.getElementById("server_response")
 
-const server_url = "placeholder"
+const server_url = "https://mvslages.com/v1";
 
 function clientResponse(text){
     server_response_div.textContent = text;
@@ -15,48 +15,50 @@ function clientResponse(text){
 insert_patient_btn.addEventListener('click', () => {
     
     // Patients to be inserted everytime the button is pressed
-    const sample_patients = [
-        {name: 'Sara Brown', date_of_birth: '1901-01-01'},
-        {name: 'John Smith', date_of_birth: '1941-01-1'},
-        {name: 'Jack Ma', date_of_birth: '1961-01-30'},
-        {name: 'Elon Musk', date_of_birth: '1999-01-01'}
-    ];
+    const sample_query = `
+    INSERT INTO Patients (name, dateOfBirth) VALUES
+    ('Sara Brown', '1901-01-01'),
+    ('John Smith', '1941-01-01'),
+    ('Jack Ma', '1961-01-30'),
+    ('Elon Musk', '1999-01-01');
+    `;
 
     // unfinished
-    fetch(``, {
+    fetch(`${server_url}/query`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify(sample_patients)
+        body: JSON.stringify({query: sample_query})
     })
-    .then(res => res.text())
-    .then(clientResponse()) // takes in res.text() (gets piped)
-    .catch(err => clientResponse(USER_MESSAGES.ERROR + err))
-});
+    .then(res => res.json())
+    .then(data => clientResponse(JSON.stringify(data)))
+    .catch(err => clientResponse(USER_MESSAGES.ERROR + err));
 
 // Part B
 submit_query_btn.addEventListener('click', () => {
     
-    const query = sql_query.value.trim();
+    const user_query = sql_query.value.trim();
 
-    if (!query){
+    if (!user_query){
         clientResponse(USER_MESSAGES.EMPTY_QUERY);
+        return;
     }
 
     // SELECT
-    if (query.toLowerCase().startsWith('select')) {
-        fetch(``)
+    if (user_query.toLowerCase().startsWith('select')) {
+        fetch(`${server_url}/query/?query=${encodeURIComponent(user_query)}`)
         .then(res => res.json())
-        // placeholder
+        .then(data => clientResponse(JSON.stringify(data)))
+        .catch(err => clientResponse(USER_MESSAGES.ERROR + err));
 
     // INSERT
-    } else if (query.toLowerCase().startsWith('insert')) {
-        fetch(`/query`, {
+    } else if (user_query.toLowerCase().startsWith('insert')) {
+        fetch(`${server_url}/query`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(query)
+            body: JSON.stringify({query: user_query})
         })
-        .then(res => res.text())
-        .then(clientResponse)
+        .then(res => res.json())
+        .then(data => clientResponse(JSON.stringify(data)))
         .catch(err => clientResponse(USER_MESSAGES.ERROR + err))
     } else {
         clientResponse(USER_MESSAGES.INVALID_QUERY)
@@ -67,4 +69,5 @@ window.onload = () => {
     insert_patient_btn.innerHTML =  USER_MESSAGES.INSERT_BUTTON;
     submit_query_btn.innerHTML = USER_MESSAGES.SUBMIT_QUERY_BTN;
 
+}
 }
