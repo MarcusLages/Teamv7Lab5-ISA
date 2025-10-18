61977e56a008f4d6909cc319f3a5fcf514c910fc
hugoamuan <hugo.amuan@icloud.com>
2025-10-18 16:08:00 -0700

    fixed path for USER_Messages for hosted version

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,17 +1,20 @@
-import { USER_MESSAGES } from "../messages/lang/en/user.js"
+import { USER_MESSAGES } from "/messages/lang/en/user.js"
 
+// DOM element references in index.html
 const insert_patient_btn = document.getElementById("insert_patients");
 const submit_query_btn = document.getElementById("submit_query")
 const sql_query = document.getElementById("sql_query");
 const server_response_div = document.getElementById("server_response")
 
+// Base domain - from backend README
 const server_url = "https://mvslages.com/v1";
 
+// Helper to display server response text to user.
 function clientResponse(text){
     server_response_div.textContent = text;
 }
 
-// Part A
+// Part A - Inserts sample patients each time button is clicked
 insert_patient_btn.addEventListener('click', () => {
     
     // Patients to be inserted everytime the button is pressed
@@ -25,26 +28,32 @@ insert_patient_btn.addEventListener('click', () => {
 
     // unfinished
     fetch(`${server_url}/query`, {
-        method: 'POST',
+        method: 'POST', // required by backend 
         headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({query: sample_query})
+        body: JSON.stringify({query: sample_query})  // backend looks for req.body.query
     })
-    .then(res => res.json())
+    .then(res => res.json()) // backend  responds with JSON format
     .then(data => clientResponse(JSON.stringify(data)))
     .catch(err => clientResponse(USER_MESSAGES.ERROR + err));
+});
 
-// Part B
+// Part B - Reads SQL queries typed by the user. Backend enforces correct
+// SQL type (403 for invalid query type, error 402 for missing or invalid SQL queries)
+// If query begins with SELECT ... send GET /query/?query=SELECT name,
+// If query begins with INSERT ... send POST /query with JSON body
 submit_query_btn.addEventListener('click', () => {
     
     const user_query = sql_query.value.trim();
 
+    // If no query was entered by the user
     if (!user_query){
         clientResponse(USER_MESSAGES.EMPTY_QUERY);
         return;
     }
 
-    // SELECT
+    // SELECT -> follow backend rule: GET /query + SQL as query param
     if (user_query.toLowerCase().startsWith('select')) {
+        // ensure special chars within a URI component (query param value) is treated as data rather than URI structure.
         fetch(`${server_url}/query/?query=${encodeURIComponent(user_query)}`)
         .then(res => res.json())
         .then(data => clientResponse(JSON.stringify(data)))
@@ -55,7 +64,7 @@ submit_query_btn.addEventListener('click', () => {
         fetch(`${server_url}/query`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({query: user_query})
+            body: JSON.stringify({query: user_query}) // json body backend expects
         })
         .then(res => res.json())
         .then(data => clientResponse(JSON.stringify(data)))
@@ -69,5 +78,4 @@ window.onload = () => {
     insert_patient_btn.innerHTML =  USER_MESSAGES.INSERT_BUTTON;
     submit_query_btn.innerHTML = USER_MESSAGES.SUBMIT_QUERY_BTN;
 
-}
 }
