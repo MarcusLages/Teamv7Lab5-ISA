ce1a43d1a45930a29d888333f29e9996d93fc7cd
MarcusLages <marcusvlages@gmail.com>
2025-10-19 13:03:37 -0700

    Took out dockerfiles

--- backend/modules/db.js
+++ backend/modules/db.js
@@ -20,6 +20,7 @@ export class PatientDB {
     constructor() {
         this.pool = mysql.createPool({
             host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWD,
             database: process.env.DB_NAME
